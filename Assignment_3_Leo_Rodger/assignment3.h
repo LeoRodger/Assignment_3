92b9a1387201cb74a6b7bdb87defd4bed17c68c0
Leo Rodger <leomrodger@gmail.com>
2016-09-11 13:04:59 +1200

    start of google style guide formatting moved include files to their own .h file

---
+++ Assignment_3_Leo_Rodger/assignment3.h
@@ -0,0 +1,16 @@
+/********************************************************
+This is the header file which contains all the include
+files for assingment 3 comparison of feacture detection
+********************************************************/
+#pragma once 
+// #pragma once is a preprocessor directive designed to cause the 
+// current source file to be included only once in a single compilation
+#include <opencv2\xfeatures2d.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
